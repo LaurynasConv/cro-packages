54bc0a2fd6ff6bc157a5f5417ad3e3b45dd8209c
Laurynas Grigutis <laurynas@conversiondev.com>
2021-05-27 00:52:26 +0300

    Poller | Force prividing an array of selectors

--- poller/index.ts
+++ poller/index.ts
@@ -2,16 +2,12 @@ if (window.NodeList && !NodeList.prototype.forEach) { NodeList.prototype.forEach
 const defaultOptions: MutationObserverInit = { childList: true, subtree: true, attributes: true, characterData: true };
 export const Observer: typeof MutationObserver = window.MutationObserver || (window as any).WebKitMutationObserver;
 
-export function waitForElement<ElementType extends HTMLElement>(selector: string, callback: (elements: NodeListOf<ElementType>) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
 export function waitForElement<ElementType extends HTMLElement>(selector: string[], callback: (...elements: NodeListOf<ElementType>[]) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
-export function waitForElement<Callback extends ObserverFn>(selector: Callback, callback: (elements: ReturnType<Callback>) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
 export function waitForElement<Callback extends ObserverFn>(selector: Callback[], callback: (...elements: ReturnType<Callback>[]) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
 export function waitForElement<ElementType extends HTMLElement, Callback extends ObserverFn>(selector: (string | Callback)[], callback: (...elements: (NodeListOf<ElementType> | ReturnType<Callback>)[]) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
 export function waitForElement<ElementType extends HTMLElement, Callback extends ObserverFn>(
-  selector: string | Callback | (string | Callback)[],
-  callback: (
-    elements: NodeListOf<ElementType> | ReturnType<Callback> | (NodeListOf<ElementType> | ReturnType<Callback>)[],
-  ) => any,
+  selector: (string | Callback)[],
+  callback: (...elements: (NodeListOf<ElementType> | ReturnType<Callback>)[]) => any,
   { timeout = 10000, container = document, options = defaultOptions }: WaitFormElementOptions = {},
 ) {
   if (Observer) {
@@ -27,21 +23,11 @@ export function waitForElement<ElementType extends HTMLElement, Callback extends
         }
       };
 
-      if (!Array.isArray(selector)) {
-        const elements = getElements(selector);
-
-        if (elements) {
-          clearTimeout(tOut);
-          observer.disconnect();
-          callback(elements);
-        }
-      } else {
-        const elements = selector.map(getElements);
-        if (!elements.some(it => it === undefined)) {
-          clearTimeout(tOut);
-          observer.disconnect();
-          callback.apply(null, elements as any); // eslint-disable-line prefer-spread
-        }
+      const elements = selector.map(getElements);
+      if (!elements.some(it => it === undefined)) {
+        clearTimeout(tOut);
+        observer.disconnect();
+        callback.apply(null, elements as any); // eslint-disable-line prefer-spread
       }
     };
     observer = new Observer(observerFn);
