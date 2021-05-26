5a414f61dc840c5b6b344a8a24c73894e5d5684d
Laurynas Grigutis <laurynas@conversiondev.com>
2021-05-27 00:32:20 +0300

    Poller | allow prividing an array of selectors

--- poller/index.ts
+++ poller/index.ts
@@ -2,11 +2,16 @@ if (window.NodeList && !NodeList.prototype.forEach) { NodeList.prototype.forEach
 const defaultOptions: MutationObserverInit = { childList: true, subtree: true, attributes: true, characterData: true };
 export const Observer: typeof MutationObserver = window.MutationObserver || (window as any).WebKitMutationObserver;
 
-export function waitForElement<ElementType extends Element>(selector: string, callback: (elements: NodeListOf<ElementType>) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
+export function waitForElement<ElementType extends HTMLElement>(selector: string, callback: (elements: NodeListOf<ElementType>) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
+export function waitForElement<ElementType extends HTMLElement>(selector: string[], callback: (...elements: NodeListOf<ElementType>[]) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
 export function waitForElement<Callback extends ObserverFn>(selector: Callback, callback: (elements: ReturnType<Callback>) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
-export function waitForElement<ElementType extends Element, Callback extends ObserverFn>(
-  selector: string | Callback,
-  callback: (elements: NodeListOf<ElementType> | ReturnType<Callback>) => any,
+export function waitForElement<Callback extends ObserverFn>(selector: Callback[], callback: (...elements: ReturnType<Callback>[]) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
+export function waitForElement<ElementType extends HTMLElement, Callback extends ObserverFn>(selector: (string | Callback)[], callback: (...elements: (NodeListOf<ElementType> | ReturnType<Callback>)[]) => any, options?: WaitFormElementOptions): MutationObserver | undefined; // eslint-disable-line prettier/prettier
+export function waitForElement<ElementType extends HTMLElement, Callback extends ObserverFn>(
+  selector: string | Callback | (string | Callback)[],
+  callback: (
+    elements: NodeListOf<ElementType> | ReturnType<Callback> | (NodeListOf<ElementType> | ReturnType<Callback>)[],
+  ) => any,
   { timeout = 10000, container = document, options = defaultOptions }: WaitFormElementOptions = {},
 ) {
   if (Observer) {
@@ -15,13 +20,28 @@ export function waitForElement<ElementType extends Element, Callback extends Obs
       observer.disconnect();
     }, timeout);
     const observerFn = (records: MutationRecord[]) => {
-      const elements =
-        typeof selector === 'string' ? container.querySelectorAll<ElementType>(selector) : selector(records);
+      const getElements = (sel: string | Callback) => {
+        const elements = typeof sel === 'string' ? container.querySelectorAll<ElementType>(sel) : sel(records);
+        if ((typeof sel === 'function' && elements !== undefined) || elements?.length) {
+          return elements;
+        }
+      };
 
-      if ((typeof selector === 'function' && elements !== undefined) || elements?.length) {
-        clearTimeout(tOut);
-        observer.disconnect();
-        callback(elements);
+      if (!Array.isArray(selector)) {
+        const elements = getElements(selector);
+
+        if (elements) {
+          clearTimeout(tOut);
+          observer.disconnect();
+          callback(elements);
+        }
+      } else {
+        const elements = selector.map(getElements);
+        if (!elements.some(it => it === undefined)) {
+          clearTimeout(tOut);
+          observer.disconnect();
+          callback.apply(null, elements as any); // eslint-disable-line prefer-spread
+        }
       }
     };
     observer = new Observer(observerFn);
