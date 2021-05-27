3014567a32710170a42d0235f02d44dac9b624b0
Laurynas Grigutis <laurynas@conversiondev.com>
2021-05-27 14:12:20 +0300

    Logger | Changed cookie check cfQA->cfQA_log

--- logger/index.ts
+++ logger/index.ts
@@ -1,6 +1,6 @@
 const ElProto = Element.prototype; ElProto.matches||(ElProto.matches=(ElProto as any).msMatchesSelector||ElProto.webkitMatchesSelector),ElProto.closest||(ElProto.closest=function(e: any){var t: any=this;do{if(ElProto.matches.call(t,e))return t}while(null!==(t=t.parentElement||t.parentNode)&&1===t.nodeType);return null}); // eslint-disable-line
 
-const isQA = /cfQA=true/.test(document.cookie + window.location.href) || localStorage.cfQA === 'true';
+const isQA = /cfQA_log=true/.test(document.cookie + window.location.href) || localStorage.cfQA_log === 'true';
 export const logger: Logger = {
   init: identifier => {
     if (isQA) {
